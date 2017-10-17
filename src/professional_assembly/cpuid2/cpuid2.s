bcfcddbeff1cfe72ba24a350c354d185d0b9cabe
Reid Paape <wrpaape@gmail.com>
2017-10-16 22:21:04 -0500

    i386 architecture of input file is incommpatible with i386:x86-64 output

--- src/professional_assembly/cpuid2/cpuid2.s
+++ src/professional_assembly/cpuid2/cpuid2.s
@@ -1,24 +1,23 @@
 # cpuid.s Sample program to extract the processor Vendor ID
 # ------------------------------------------------------------------------------
+.code32
 .section .data
 output:
     .asciz "The Processor Vendor ID is '%s'\n"
-
 .section .bss
     .lcomm buffer, 12
-
 .section .text
 .globl _start
 _start:
     movl $0, %eax
     cpuid
-    movl $output, %edi
-    movl %ebx,  (%edi)
+    movl $buffer, %edi
+    movl %ebx, (%edi)
     movl %edx, 4(%edi)
     movl %ecx, 8(%edi)
-    push $buffer
-    push $output
+    pushl $buffer
+    pushl $output
     call printf
     addl $8, %esp
-    push $0
+    pushl $0
     call exit
---
+++ src/professional_assembly/cpuid2/ooga.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int
+main()
+{
+	(void) puts("booga");
+	return 0;
+}
